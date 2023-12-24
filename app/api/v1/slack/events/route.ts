195580a7a7a60a0ab9ba0378b4443e290bd879a4
rkotzy <rkotzy@gmail.com>
2023-12-23 16:07:49 -0800

    stringify-logs

--- app/api/v1/slack/events/route.ts
+++ app/api/v1/slack/events/route.ts
@@ -6,7 +6,7 @@ export const runtime = 'edge';
 
 export async function POST(request: NextRequest) {
   const requestBody = await request.json();
-
+  console.log(JSON.stringify(requestBody, null, 2));
   // Check if this is a URL verification request from Slack
   if (requestBody.type === 'url_verification') {
     // Respond with the challenge value

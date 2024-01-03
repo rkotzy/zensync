4dc0d49317e655022eda6beb1d086cb5cf776728
Ryan Kotzebue <ryan@Ryans-Air-2.attlocal.net>
2024-01-03 15:30:20 -0800

    nav bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,12 @@
 // Import the base CSS styles for the radix-ui components.
-import "@radix-ui/themes/styles.css";
 import "./globals.css"
 import { Inter as FontSans } from "next/font/google"
 import { cn } from "../lib/utils";
 import type { Metadata } from "next";
-import { Theme, Card, Container, Flex } from "@radix-ui/themes";
 import Link from "next/link";
 import { Footer } from "../components/footer";
-import { Button } from "@/components/ui/button"
 import { SignInButton } from "../components/ui/sign-in-button";
+import { MainNav } from "@/components/main-nav";
 
 export const metadata: Metadata = {
   title: "Slack-to-Zendesk",
@@ -34,49 +32,16 @@ export default function RootLayout({
         fontSans.variable
       )}
       >
-        <Theme accentColor="iris" style={{ backgroundColor: "var(--gray-1)" }}>
-          <Container px="5">
-            <Flex align="center" style={{ height: "100vh" }} py="9">
-              <Flex
-                direction="column"
-                style={{
-                  height: "100%",
-                  maxHeight: 850,
-                  minHeight: 500,
-                  width: "100%",
-                }}
-                gap="5"
-              >
-                <Flex grow="1">
-                  <Card size="4" style={{ width: "100%" }}>
-                    <Flex direction="column" height="100%">
-                      <Flex asChild justify="between">
-                        <header>
-                          <Flex gap="4">
-                            <Button variant="secondary">
-                              <Link href="/">Home</Link>
-                            </Button>
-
-                            <Button variant="secondary">
-                              <Link href="/account">Account</Link>
-                            </Button>
-                          </Flex>
-
-                          <SignInButton />
-                        </header>
-                      </Flex>
-
-                      <Flex grow="1" align="center" justify="center">
-                        <main>{children}</main>
-                      </Flex>
-                    </Flex>
-                  </Card>
-                </Flex>
-                <Footer />
-              </Flex>
-            </Flex>
-          </Container>
-        </Theme>
+      <div className="border-b">
+          <div className="flex h-16 items-center px-4">
+          <Link href="/" className="text-2xl font-bold tracking-tight">Slack-to-Zendesk</Link>
+            <MainNav className="mx-6" />
+            <div className="ml-auto flex items-center space-x-4">
+              <SignInButton />
+            </div>
+          </div>
+      </div>
+      <div className="flex-1 lg:max-w-2xl">{children}</div>
       </body>
     </html>
   );

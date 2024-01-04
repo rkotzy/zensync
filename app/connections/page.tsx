f217a0b302b24f1fab0ba9823eebd0e249f797dd
rkotzy <rkotzy@gmail.com>
2024-01-04 15:41:31 -0800

    update callback to same window

--- app/connections/page.tsx
+++ app/connections/page.tsx
@@ -1,25 +1,28 @@
-import { getUser } from "../../auth";
-import { buttonVariants } from "@/components/ui/button"
-import Link from "next/link";
+'use client';
 
+import { buttonVariants } from '@/components/ui/button';
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
 
-export default async function ConnectionsPage() {
-  const { user } = await getUser();
+export default function ConnectionsPage() {
+  const searchParams = useSearchParams();
 
-  const userFields = user && [
-    ["First name", user.firstName],
-    ["Last name", user.lastName],
-    ["Email", user.email],
-    ["Id", user.id],
-  ];
+  const slackOauthStatus = searchParams.get('slackOauth');
+  const message = searchParams.get('message');
 
   return (
     <>
       <h1>Connections</h1>
-      <Link href="https://zensync.vercel.app/api/v1/slack/auth/11111111-1111-1111-1111-111111111111/redirect"
-      target="_blank" 
-      rel="noopener noreferrer"
-      className={buttonVariants({ variant: "outline" })}>Connect To Slack</Link>
+      <Link
+        href="https://zensync.vercel.app/api/v1/slack/auth/11111111-1111-1111-1111-111111111111/redirect"
+        className={buttonVariants({ variant: 'outline' })}
+      >
+        Connect To Slack
+      </Link>
+      {slackOauthStatus === 'success' && (
+        <div>Successfully connected to Slack.</div>
+      )}
+      {slackOauthStatus === 'error' && <div>Error: {message}</div>}
     </>
   );
 }

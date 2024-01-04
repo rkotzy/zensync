ed43b6da5b011383616a0f7627a9bd3d90565439
Ryan Kotzebue <ryan@Ryans-Air-2.attlocal.net>
2024-01-03 16:21:33 -0800

    started connections page

--- app/connections/page.tsx
+++ app/connections/page.tsx
@@ -1,4 +1,7 @@
 import { getUser } from "../../auth";
+import { buttonVariants } from "@/components/ui/button"
+import Link from "next/link";
+
 
 export default async function ConnectionsPage() {
   const { user } = await getUser();
@@ -13,6 +16,10 @@ export default async function ConnectionsPage() {
   return (
     <>
       <h1>Connections</h1>
+      <Link href="https://zensync.vercel.app/api/v1/slack/auth/11111111-1111-1111-1111-111111111111/redirect"
+      target="_blank" 
+      rel="noopener noreferrer"
+      className={buttonVariants({ variant: "outline" })}>Connect To Slack</Link>
     </>
   );
 }

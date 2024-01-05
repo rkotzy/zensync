8cae4109c36a698fb4b93360676e6a437197080e
rkotzy <rkotzy@gmail.com>
2024-01-04 16:13:05 -0800

    zendesk ui

--- app/connections/page.tsx
+++ app/connections/page.tsx
@@ -1,5 +1,6 @@
 'use client';
-
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
 import { buttonVariants } from '@/components/ui/button';
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
@@ -13,9 +14,10 @@ export default function ConnectionsPage() {
   return (
     <>
       <h1>Connections</h1>
+      <h2>Slack</h2>
       <Link
         href="https://zensync.vercel.app/api/v1/slack/auth/11111111-1111-1111-1111-111111111111/redirect"
-        className={buttonVariants({ variant: 'outline' })}
+        className={buttonVariants()}
       >
         Connect To Slack
       </Link>
@@ -23,6 +25,16 @@ export default function ConnectionsPage() {
         <div>Successfully connected to Slack.</div>
       )}
       {slackOauthStatus === 'error' && <div>Error: {message}</div>}
+
+      <h2>Zendesk</h2>
+      <Label htmlFor="text">Zendesk Email Address</Label>
+      <Input type="text" id="zendesk-email" placeholder="Email Address" />
+
+      <Label htmlFor="text">Zendesk API Key</Label>
+      <Input type="text" id="zendesk-key" placeholder="API Key" />
+      <Link href="/" className={buttonVariants()}>
+        Test Connection
+      </Link>
     </>
   );
 }

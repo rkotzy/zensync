8f486f4eab72253323bbfd51a4f38f5f0ae42b34
Ryan Kotzebue <ryan@Ryans-MacBook-Air-2.local>
2024-01-08 15:47:02 -0800

    zd connection test

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.ctfassets.net'],
+    domains: ['images.ctfassets.net']
   },
-}
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://zensync.vercel.app/api/:path*' // Proxy to external API
+      }
+    ];
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

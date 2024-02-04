49fa168f7a5b143b47d688fecf598368b466cd27
rkotzy <rkotzy@gmail.com>
2024-02-04 15:44:18 -0800

    neon driver

--- lib/drizzle.ts
+++ lib/drizzle.ts
@@ -1,6 +1,7 @@
-import { drizzle } from 'drizzle-orm/vercel-postgres';
-import { sql } from '@vercel/postgres';
+import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
 import * as schema from './schema';
 
 // Use this object to send drizzle queries to your DB
+const sql = neon(process.env.POSTGRES_URL!);
 export const db = drizzle(sql, { schema: schema });

b8b8c8983136d070e435fa6d89abb27021a9ef2b
rkotzy <rkotzy@gmail.com>
2024-02-06 21:35:17 -0800

    added logging

--- zensync-workers/custom-env.d.ts
+++
@@ -1,3 +0,0 @@
-declare const POSTGRES_URL: string;
-declare const SLACK_SIGNING_SECRET: string;
-declare const BETTER_STACK_SOURCE_TOKEN: string;
--- zensync-workers/src/endpoints/zendeskEvents.ts
+++ zensync-workers/src/endpoints/zendeskEvents.ts
@@ -1,4 +1,5 @@
-import { db } from '@/lib/drizzle';
+import { OpenAPIRoute } from '@cloudflare/itty-router-openapi';
+import { initializeDb } from '@/lib/drizzle';
 import { eq } from 'drizzle-orm';
 import {
   zendeskConnection,
@@ -6,125 +7,133 @@ import {
   slackConnection,
   conversation
 } from '@/lib/schema';
+import * as schema from '@/lib/schema';
 import { SlackResponse } from '@/interfaces/slack-api.interface';
 import { ZendeskEvent } from '@/interfaces/zendesk-api.interface';
 import { Logtail } from '@logtail/edge';
 import { EdgeWithExecutionContext } from '@logtail/edge/dist/es6/edgeWithExecutionContext';
+import { NeonHttpDatabase } from 'drizzle-orm/neon-http';
 
 export interface Env {
-  // ...
+  BETTER_STACK_SOURCE_TOKEN: string;
 }
 
-export const baseLogger = new Logtail(BETTER_STACK_SOURCE_TOKEN);
-
-export const handler = async (
-  request: Request,
-  env: Env,
-  ctx: ExecutionContext
-): Promise<Response> => {
-  const logger = baseLogger.withExecutionContext(ctx);
-  const requestBody = (await request.json()) as ZendeskEvent;
-  logger.info(JSON.stringify(requestBody, null, 2));
-
-  // Save some database calls if it's a message from Zensync
-
-  // Ignore messages from Zensync
-  if (
-    typeof requestBody.current_user_external_id === 'string' &&
-    requestBody.current_user_external_id.startsWith('zensync')
+export class ZendeskEventHandler extends OpenAPIRoute {
+  async handle(
+    request: Request,
+    env: any,
+    context: any,
+    data: Record<string, any>
   ) {
-    logger.info('Message from Zensync, skipping');
-    return new Response('Ok', { status: 200 });
-  }
+    // Set up logger right away
+    const baseLogger = new Logtail(env.BETTER_STACK_SOURCE_TOKEN);
+    const logger = baseLogger.withExecutionContext(context);
 
-  // Make sure we have the last updated ticket time
-  const ticketLastUpdatedAt = requestBody.last_updated_at;
-  if (!ticketLastUpdatedAt) {
-    logger.error('Missing last_updated_at');
-    return new Response('Missing last_updated_at', { status: 400 });
-  }
+    // Initialize the database
+    const db = initializeDb(env);
 
-  // Ignore messages if last_updated_at === created_at
-  // WARNING: - This would ignore messages sent in same minute.
-  // Should log in Sentry probably?
-  if (requestBody.last_updated_at === requestBody.created_at) {
-    logger.info('Message is not an update, skipping');
-    return new Response('Ok', { status: 200 });
-  }
+    const requestBody = (await request.json()) as ZendeskEvent;
+    logger.info(JSON.stringify(requestBody, null, 2));
+    // Save some database calls if it's a message from Zensync
 
-  // Authenticate the request and get slack connection Id
-  const slackConnectionId = await authenticateRequest(request, logger);
-  if (!slackConnectionId) {
-    return new Response('Unauthorized', { status: 401 });
-  }
-
-  // Get the conversation from external_id
-  const conversationInfo = await db.query.conversation.findFirst({
-    where: eq(conversation.id, requestBody.external_id),
-    with: {
-      channel: true
+    // Ignore messages from Zensync
+    if (
+      typeof requestBody.current_user_external_id === 'string' &&
+      requestBody.current_user_external_id.startsWith('zensync')
+    ) {
+      logger.info('Message from Zensync, skipping');
+      return new Response('Ok', { status: 200 });
     }
-  });
 
-  if (!conversationInfo?.slackParentMessageId) {
-    logger.error(`No conversation found for id ${requestBody.external_id}`);
-    return new Response('No conversation found', { status: 404 });
-  }
+    // Make sure we have the last updated ticket time
+    const ticketLastUpdatedAt = requestBody.last_updated_at;
+    if (!ticketLastUpdatedAt) {
+      logger.error('Missing last_updated_at');
+      return new Response('Missing last_updated_at', { status: 400 });
+    }
 
-  logger.info(
-    `ConversationInfo retrieved: ${JSON.stringify(conversationInfo)}`
-  );
+    // Ignore messages if last_updated_at === created_at
+    // WARNING: - This would ignore messages sent in same minute.
+    // Should log in Sentry probably?
+    if (requestBody.last_updated_at === requestBody.created_at) {
+      logger.info('Message is not an update, skipping');
+      return new Response('Ok', { status: 200 });
+    }
 
-  // To be safe I should double-check the organization_id owns the channel_id
-  if (
-    !conversationInfo.channel ||
-    !conversationInfo.channel.slackChannelIdentifier ||
-    conversationInfo.channel.slackConnectionId !== slackConnectionId
-  ) {
-    logger.warn(`Invalid Ids: ${slackConnectionId} !== ${conversationInfo}`);
-    return new Response('Invalid Ids', { status: 401 });
-  }
+    // Authenticate the request and get slack connection Id
+    const slackConnectionId = await authenticateRequest(request, db, logger);
+    if (!slackConnectionId) {
+      return new Response('Unauthorized', { status: 401 });
+    }
 
-  // To be safe I should double-check the organization_id owns the channel_id
-  if (
-    !conversationInfo.channel ||
-    !conversationInfo.channel.slackChannelIdentifier ||
-    conversationInfo.channel.slackConnectionId !== slackConnectionId
-  ) {
-    logger.warn(`Invalid Ids: ${slackConnectionId} !== ${conversationInfo}`);
-    return new Response('Invalid Ids', { status: 401 });
-  }
-
-  // Get the full slack connection info
-  const slackConnectionInfo: SlackConnection | undefined =
-    await db.query.slackConnection.findFirst({
-      where: eq(slackConnection.id, slackConnectionId)
+    // Get the conversation from external_id
+    const conversationInfo = await db.query.conversation.findFirst({
+      where: eq(conversation.id, requestBody.external_id),
+      with: {
+        channel: true
+      }
     });
 
-  if (!slackConnectionInfo) {
-    logger.error(`No Slack connection found for id ${slackConnectionId}`);
-    return new Response('No Slack connection found', { status: 404 });
-  }
+    if (!conversationInfo?.slackParentMessageId) {
+      logger.error(`No conversation found for id ${requestBody.external_id}`);
+      return new Response('No conversation found', { status: 404 });
+    }
 
-  logger.info(
-    `SlackConnectionInfo retrieved: ${JSON.stringify(slackConnectionInfo)}`
-  );
-
-  try {
-    await sendSlackMessage(
-      requestBody,
-      slackConnectionInfo,
-      conversationInfo.slackParentMessageId,
-      conversationInfo.channel.slackChannelIdentifier,
-      logger
+    logger.info(
+      `ConversationInfo retrieved: ${JSON.stringify(conversationInfo)}`
     );
-  } catch (error) {
-    logger.error(error);
-    return new Response('Error', { status: 500 });
-  }
 
-  return new Response('Ok', { status: 202 });
-};
+    // To be safe I should double-check the organization_id owns the channel_id
+    if (
+      !conversationInfo.channel ||
+      !conversationInfo.channel.slackChannelIdentifier ||
+      conversationInfo.channel.slackConnectionId !== slackConnectionId
+    ) {
+      logger.warn(`Invalid Ids: ${slackConnectionId} !== ${conversationInfo}`);
+      return new Response('Invalid Ids', { status: 401 });
+    }
+
+    // To be safe I should double-check the organization_id owns the channel_id
+    if (
+      !conversationInfo.channel ||
+      !conversationInfo.channel.slackChannelIdentifier ||
+      conversationInfo.channel.slackConnectionId !== slackConnectionId
+    ) {
+      logger.warn(`Invalid Ids: ${slackConnectionId} !== ${conversationInfo}`);
+      return new Response('Invalid Ids', { status: 401 });
+    }
+
+    // Get the full slack connection info
+    const slackConnectionInfo: SlackConnection | undefined =
+      await db.query.slackConnection.findFirst({
+        where: eq(slackConnection.id, slackConnectionId)
+      });
+
+    if (!slackConnectionInfo) {
+      logger.error(`No Slack connection found for id ${slackConnectionId}`);
+      return new Response('No Slack connection found', { status: 404 });
+    }
+
+    logger.info(
+      `SlackConnectionInfo retrieved: ${JSON.stringify(slackConnectionInfo)}`
+    );
+
+    try {
+      await sendSlackMessage(
+        requestBody,
+        slackConnectionInfo,
+        conversationInfo.slackParentMessageId,
+        conversationInfo.channel.slackChannelIdentifier,
+        logger
+      );
+    } catch (error) {
+      logger.error(error);
+      return new Response('Error', { status: 500 });
+    }
+
+    return new Response('Ok', { status: 202 });
+  }
+}
 
 async function getSlackUser(
   connection: SlackConnection,
@@ -165,6 +174,7 @@ async function getSlackUser(
 
 async function authenticateRequest(
   request: Request,
+  db: NeonHttpDatabase<typeof schema>,
   logger: EdgeWithExecutionContext
 ): Promise<string | null> {
   const authorizationHeader = request.headers.get('authorization');

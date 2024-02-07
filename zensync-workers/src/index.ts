713b04b63f01e3171f662b670b8822c3da6652a9
rkotzy <rkotzy@gmail.com>
2024-02-06 22:45:54 -0800

    remove api from route

--- zensync-workers/src/index.ts
+++ zensync-workers/src/index.ts
@@ -6,10 +6,10 @@ import { SlackAuthCallback } from './endpoints/slackAuthCallback';
 
 export const router = OpenAPIRouter();
 
-router.post(`/api/v1/zendesk/events`, ZendeskEventHandler);
-router.post(`/api/v1/slack/interactivity`, SlackInteractivityHandler);
-router.get(`/api/v1/slack/auth/redirect`, SlackAuthRedirect);
-router.get(`/api/v1/slack/auth/callback`, SlackAuthCallback);
+router.post(`/v1/zendesk/events`, ZendeskEventHandler);
+router.post(`/v1/slack/interactivity`, SlackInteractivityHandler);
+router.get(`/v1/slack/auth/redirect`, SlackAuthRedirect);
+router.get(`/v1/slack/auth/callback`, SlackAuthCallback);
 //router.post(`/api/v1/slack/events`, );
 
 // Queue consumers

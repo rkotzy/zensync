cd1cff3406156133af6125993665aa640068841c
rkotzy <rkotzy@gmail.com>
2024-02-06 22:09:58 -0800

    set up interactivity route

--- zensync-workers/src/index.ts
+++ zensync-workers/src/index.ts
@@ -1,9 +1,11 @@
 import { OpenAPIRouter } from '@cloudflare/itty-router-openapi';
 import { ZendeskEventHandler } from './endpoints/zendeskEvents';
+import { SlackInteractivityHandler } from './endpoints/slackInteractivity';
 
 export const router = OpenAPIRouter();
 
 router.post(`/api/v1/zendesk/events`, ZendeskEventHandler);
+router.post(`/api/v1/slack/interactivity`, SlackInteractivityHandler);
 
 // 404 for everything else
 router.all('*', () =>

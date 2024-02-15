d812a28c09675f9ddb5d5c433742879f82a27b24
rkotzy <rkotzy@gmail.com>
2024-02-14 21:55:24 -0800

    new slack connection queue boilerplate

---
+++ zensync-workers/src/queues/slackConnectionCreated.ts
@@ -0,0 +1,10 @@
+import { Env } from '@/interfaces/env.interface';
+import { EdgeWithExecutionContext } from '@logtail/edge/dist/es6/edgeWithExecutionContext';
+
+export async function uploadFilesToZendesk(
+  requestJson: any,
+  env: Env,
+  logger: EdgeWithExecutionContext
+) {
+  logger.info(JSON.stringify(requestJson, null, 2));
+}

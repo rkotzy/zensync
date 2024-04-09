46296355d1cee88717694dd8f95569fff223bf7c
rkotzy <rkotzy@gmail.com>
2024-04-08 21:20:36 -0700

    hyperdrive binding

--- zensync-workers/lib/drizzle.ts
+++ zensync-workers/lib/drizzle.ts
@@ -1,9 +1,10 @@
 import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
 import * as schema from './schema';
+import { Env } from '@/interfaces/env.interface';
 
-export function initializeDb(env) {
-  const sql = neon(env.POSTGRES_URL);
+export function initializeDb(env: Env) {
+  const sql = neon(env.HYPERDRIVE.connectionString);
   const db = drizzle(sql, { schema: schema });
   return db;
 }

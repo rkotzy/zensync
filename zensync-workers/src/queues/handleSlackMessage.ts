97a7c4202d7de8a5ae007595ffb78475b294a80b
Ryan Kotzebue <ryan@Ryans-Air-2.attlocal.net>
2024-04-11 16:15:29 -0700

    updated message formatter

--- zensync-workers/src/queues/handleSlackMessage.ts
+++ zensync-workers/src/queues/handleSlackMessage.ts
@@ -42,6 +42,10 @@ import {
 } from '@/interfaces/global-settings.interface';
 import { initializeDb } from '@/lib/database';
 import { postEphemeralMessage, getSlackUser } from '@/lib/slack-api';
+import {
+  slackMarkdownToHtml,
+  generateHTMLPermalink
+} from '@/lib/message-formatters';
 
 const MISSING_ZENDESK_CREDENTIALS_MESSAGE =
   'Zendesk credentials are missing or inactive. Configure them in the Zensync app settings to start syncing messages.';
@@ -1228,68 +1232,4 @@ async function sameSenderInTimeframeConversation(
     );
     return null;
   }
-}
-
-function generateHTMLPermalink(
-  slackConnection: SlackConnection,
-  messageData: SlackMessageData
-): string {
-  return `<p><i>(<a href="https://${
-    slackConnection.domain
-  }.slack.com/archives/${messageData.channel}/p${messageData.ts.replace(
-    '.',
-    ''
-  )}">View in Slack</a>)</i></p>`;
-}
-
-function slackMarkdownToHtml(markdown: string): string {
-  // Handle block quotes
-  markdown = markdown.replace(/^>\s?(.*)/gm, '<blockquote>$1</blockquote>');
-
-  // Handle code blocks first to prevent formatting inside them
-  markdown = markdown.replace(
-    /```(.*?)```/gs,
-    (_, code) => `<pre><code>${escapeCurlyBraces(code)}</code></pre>`
-  );
-
-  // Handle ordered lists
-  markdown = markdown.replace(
-    /^\d+\.\s(.*)/gm,
-    (_, item) => `<li>${item}</li>`
-  );
-  markdown = markdown.replace(/(<li>.*<\/li>)/gs, '<ol>$1</ol>');
-
-  // Handle bulleted lists
-  markdown = markdown.replace(
-    /^[\*\+\-]\s(.*)/gm,
-    (_, item) => `<li>${item}</li>`
-  );
-  markdown = markdown.replace(/(<li>.*<\/li>)/gs, '<ul>$1</ul>');
-
-  // Handle inline code
-  markdown = markdown.replace(
-    /`(.*?)`/g,
-    (_, code) => `<code>${escapeCurlyBraces(code)}</code>`
-  );
-
-  // Convert bold text: *text*
-  markdown = markdown.replace(/\*(.*?)\*/g, '<strong>$1</strong>');
-
-  // Convert italic text: _text_
-  markdown = markdown.replace(/_(.*?)_/g, '<em>$1</em>');
-
-  // Convert strikethrough: ~text~
-  markdown = markdown.replace(/~(.*?)~/g, '<del>$1</del>');
-
-  // Convert new lines to <br> for lines not inside block elements
-  markdown = markdown.replace(
-    /^(?!<li>|<\/li>|<ol>|<\/ol>|<ul>|<\/ul>|<pre>|<\/pre>|<blockquote>|<\/blockquote>).*$/gm,
-    '$&<br>'
-  );
-
-  return markdown;
-}
-
-function escapeCurlyBraces(code: string): string {
-  return code.replace(/{{(.*?)}}/g, '&lcub;&lcub;$1&rcub;&rcub;');
 }
